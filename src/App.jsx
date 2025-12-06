2dc86ea7c6e2df014128f88d541c0541334be65e
Alaf Baig <mirzaalaf21@gmai.com>
2025-12-06 22:03:05 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 function App() {
- return <div>
+ return (
+ <div>
     <h1>hello</h1>
     <h2>alaf</h2>
  </div>
+ );
 }
 
 export default App
