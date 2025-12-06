68fdc13e7483dfcc71ffa3b6621e93b321a4cf01
Alaf Baig <mirzaalaf21@gmai.com>
2025-12-06 21:50:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+ return <div>
+    <h1>hello</h1>
+    <h2>alaf</h2>
+ </div>
+}
+
+export default App
